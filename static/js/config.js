93dc05037714cc78fae500a438154b550fbe1da3
harmonic-lqw <52872613+harmonic-lqw@users.noreply.github.com>
2023-05-15 11:02:53 +0800

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+const config = {
+    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
+    url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
+    apiKey: "c2stVFhQZXpzR3daR2I2VklHS2VRanRUM0JsYmtGSnh6Q2dUcU9aUkIzTEtZMXAzdzBC"  // openAi Base64 编码 apiKey
+};  
