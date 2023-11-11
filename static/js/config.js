0a3ef0d8167e9cc90f209e6eb5e54cb301240b81
harmonic-lqw <52872613+harmonic-lqw@users.noreply.github.com>
2023-11-11 09:38:14 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "c2stWEJZTkZHR0ZpTmN4cmgyV1Y3eHNUM0JsYmtGSlQ1MkRLTUpNU1ZFbGQ5dGJjQXhB"  // openAi Base64 编码 apiKey
+    apiKey: "c2stZVZRNUU4M09pclo4NFl1eW9HZ3dUM0JsYmtGSnFkT3Y1b3Foa3hnc3h0aGp6cnJR"  // openAi Base64 编码 apiKey
 };
